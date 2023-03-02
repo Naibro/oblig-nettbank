83c350d725c54cb66481d49698ee8aa040b6d5ce
Kevin L. A <kevin.amu23@gmail.com>
2023-03-02 19:40:50 +0100

    endring til _loggetInn og _IkkeLoggetInn tester
    i stedet for OK og Feil
    
    la også inn skjelett for tester av de resterende metodene

--- src/test/java/oslomet/testing/EnhetstestBankController.java
+++ src/test/java/oslomet/testing/EnhetstestBankController.java
@@ -35,9 +35,8 @@ public class EnhetstestBankController {
     // denne skal Mock'es
     private Sikkerhet sjekk;
 
-    //test av hentTransaksjoner
     @Test
-    public void test_hentTransaksjonerOK(){
+    public void test_hentTransaksjonerLoggetInn(){
         // arrange
         List<Transaksjon> test = new ArrayList<>();
         Konto randomKonto = new Konto("01010110523","105010123456",619.50,"Lønnskonto","NOK",test);
@@ -52,89 +51,13 @@ public class EnhetstestBankController {
         assertEquals(randomKonto, resultat);
     }
 
-    //test av hentTransaksjoner
     @Test
-    public void test_hentTransaksjonerFeil(){
-        // arrange
-        List<Transaksjon> test = new ArrayList<>();
-        Konto randomKonto = new Konto("01010110523","105010123456",619.50,"Lønnskonto","NOK",test);
-
-        when(sjekk.loggetInn()).thenReturn("01010110523");
-        when(repository.hentTransaksjoner("105010123456","2002-04-02","2023-01-01")).thenReturn(randomKonto);
-
-        // act (f.eks feil kontonummer)
-        Konto resultat = bankController.hentTransaksjoner("10501012345","2002-04-02","2023-01-01");
-
-        // assert
-        assertNull(resultat);
-    }
-
-    @Test
-    public void hentSaldi_OK(){
-        // arrange
-        List<Konto> konto = new ArrayList<>();
-        Konto konto1 = new Konto("105010123456", "01010110523",
-                720, "Lønnskonto", "NOK", null);
-        Konto konto2 = new Konto("105010123456", "12345678901",
-                1000, "Lønnskonto", "NOK", null);
-        konto.add(konto1);
-        konto.add(konto2);
-
-        when(sjekk.loggetInn()).thenReturn("01010110523");
-        when(repository.hentSaldi(anyString())).thenReturn(konto);
-
-        // act
-        List<Konto> resultat = bankController.hentSaldi();
-
-        // assert
-        assertEquals(konto, resultat);
-    }
-
-    @Test
-    public void hentSaldi_Feil(){
-        // arrange
-        List<Konto> konto = new ArrayList<>();
-        Konto konto1 = new Konto("105010123456", "01010110523",
-                720, "Lønnskonto", "NOK", null);
-        konto.add(konto1);
-
-        when(sjekk.loggetInn()).thenReturn("01010110523");
-        when(repository.hentSaldi(anyString())).thenReturn(konto);
-
-        // act
-        List<Konto> resultat = bankController.hentSaldi();
-
-        // assert
-        assertEquals(konto, resultat);
-    }
-
-    @Test
-    public void hentKundeInfo_loggetInn() {
-
-        // arrange
-        Kunde enKunde = new Kunde("01010110523",
-                "Lene", "Jensen", "Askerveien 22", "3270",
-                "Asker", "22224444", "HeiHei");
-
-        when(sjekk.loggetInn()).thenReturn("01010110523");
-
-        when(repository.hentKundeInfo(anyString())).thenReturn(enKunde);
-
-        // act
-        Kunde resultat = bankController.hentKundeInfo();
-
-        // assert
-        assertEquals(enKunde, resultat);
-    }
-
-    @Test
-    public void hentKundeInfo_IkkeloggetInn() {
-
+    public void test_hentTransaksjonerIkkeLoggetInn(){
         // arrange
         when(sjekk.loggetInn()).thenReturn(null);
 
-        //act
-        Kunde resultat = bankController.hentKundeInfo();
+        // act
+        Konto resultat = bankController.hentTransaksjoner("105010123456","2002-04-02","2023-01-01");
 
         // assert
         assertNull(resultat);
@@ -173,6 +96,108 @@ public class EnhetstestBankController {
 
         // assert
         assertNull(resultat);
+    }
+
+    @Test
+    public void hentSaldi_loggetInn(){
+        // arrange
+        List<Konto> konto = new ArrayList<>();
+        Konto konto1 = new Konto("105010123456", "01010110523",
+                720, "Lønnskonto", "NOK", null);
+        Konto konto2 = new Konto("105010123456", "12345678901",
+                1000, "Lønnskonto", "NOK", null);
+        konto.add(konto1);
+        konto.add(konto2);
+
+        when(sjekk.loggetInn()).thenReturn("01010110523");
+        when(repository.hentSaldi(anyString())).thenReturn(konto);
+
+        // act
+        List<Konto> resultat = bankController.hentSaldi();
+
+        // assert
+        assertEquals(konto, resultat);
+    }
+
+    @Test
+    public void hentSaldi_IkkeLoggetInn(){
+        // arrange
+        when(sjekk.loggetInn()).thenReturn(null);
+
+        // act
+        List<Konto> resultat = bankController.hentSaldi();
+
+        // assert
+        assertNull(resultat);
+    }
+
+    @Test
+    public void registrerBetaling_loggetInn(){
+
+    }
+
+    @Test
+    public void registrerBetaling_IkkeLoggetInn(){
+
+    }
+
+    @Test
+    public void hentBetaling_loggetInn(){
+
+    }
+
+    @Test
+    public void hentBetaling_IkkeLoggetInn(){
+
+    }
+
+    @Test
+    public void utforBetaling_loggetInn(){
+
+    }
+
+    @Test
+    public void utforBetaling_IkkeLoggetInn(){
+
+    }
+
+    @Test
+    public void hentKundeInfo_loggetInn() {
+        // arrange
+        Kunde enKunde = new Kunde("01010110523",
+                "Lene", "Jensen", "Askerveien 22", "3270",
+                "Asker", "22224444", "HeiHei");
+
+        when(sjekk.loggetInn()).thenReturn("01010110523");
+        when(repository.hentKundeInfo(anyString())).thenReturn(enKunde);
+
+        // act
+        Kunde resultat = bankController.hentKundeInfo();
+
+        // assert
+        assertEquals(enKunde, resultat);
+    }
+
+    @Test
+    public void hentKundeInfo_IkkeloggetInn() {
+        // arrange
+        when(sjekk.loggetInn()).thenReturn(null);
+
+        //act
+        Kunde resultat = bankController.hentKundeInfo();
+
+        // assert
+        assertNull(resultat);
+    }
+
+    @Test
+    public void endreKundeInfo_loggetInn(){
+
+    }
+
+    @Test
+    public void endreKundeInfo_IkkeLoggetInn(){
+
     }
 }
 

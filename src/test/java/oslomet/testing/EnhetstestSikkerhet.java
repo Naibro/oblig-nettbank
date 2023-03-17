5b8ded866ac8f02610bedb38c56fbec190f69e6d
David Knotten <89570587+PresidentZeus@users.noreply.github.com>
2023-03-17 10:57:04 +0100

    Sikkerhet mangler admin

--- src/test/java/oslomet/testing/EnhetstestSikkerhet.java
+++ src/test/java/oslomet/testing/EnhetstestSikkerhet.java
@@ -1,13 +1,27 @@
 package oslomet.testing;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.invocation.InvocationOnMock;
 import org.mockito.junit.MockitoJUnitRunner;
 
+import org.mockito.stubbing.Answer;
+import org.springframework.mock.web.MockHttpSession;
 import oslomet.testing.DAL.BankRepository;
 import oslomet.testing.Sikkerhet.Sikkerhet;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.doAnswer;
+import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
 public class EnhetstestSikkerhet {
@@ -17,4 +31,80 @@ public class EnhetstestSikkerhet {
 
     @Mock
     private BankRepository repository;
+
+    @Mock
+    MockHttpSession session;
+
+    @Before
+    public void initSession() {
+        Map<String, Object> attributes = new HashMap<String, Object>();
+
+        doAnswer(new Answer<Object>(){
+            @Override
+            public Object answer(InvocationOnMock invocation) throws Throwable {
+                String key = (String) invocation.getArguments()[0];
+                return attributes.get(key);
+            }
+        }).when(session).getAttribute(anyString());
+
+        // Slett under!!
+        doAnswer(new Answer<Object>(){
+            @Override
+            public Object answer(InvocationOnMock invocation) throws Throwable {
+                String key = (String) invocation.getArguments()[0];
+                Object value = invocation.getArguments()[1];
+                attributes.put(key, value);
+                return null;
+            }
+        }).when(session).setAttribute(anyString(), any());
+    }
+
+    @Test
+    public void test_sjekkLoggInn() {
+        // arrange
+        when(repository.sjekkLoggInn(anyString(), anyString())).thenReturn("OK");
+
+        //session.setAttribute("Innlogget", "01010110523");
+
+        // act
+        String resultat = sikkerhet.sjekkLoggInn("01010110523","damni!");
+        // assert
+        assertEquals("OK", resultat);
+    }
+
+    @Test
+    public void test_loggetInn() {
+        // arrange
+        session.setAttribute("Innlogget","01010110523");
+
+        // act
+        String resultat = sikkerhet.loggetInn();
+
+        // Assert
+        assertEquals("01010110523", resultat);
+    }
+
+    @Test
+    public void resr_loggetInn_IkkeLoggetInn() {
+        //  arrange
+        session.setAttribute("Innlogget", null);
+
+        // act
+        String resultat = sikkerhet.loggetInn();
+
+        // assert
+        assertNull(resultat);
+    }
+
+    @Test
+    public void test_loggInnAdmin() {
+        // arrange
+        when(repository.loggInnAdmin(anyString(), anyString())).thenReturn("Admin");
+
+        // act
+        String resultat = sikkerhet.loggInnAdmin("Admin0123", "blehhh");
+
+        // assert
+        assertEquals("Admin", resultat);
+    }
 }
